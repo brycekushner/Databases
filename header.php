849f73d914d0b210cd9225160b561418f5511bd1
Cameron Lydon <cwly222@uky.edu>
2019-12-09 21:20:37 -0500

    fixed style inclusion

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <head>
-<link rel="stylesheet" type="text/css" ref="style.css" />
+<link rel="stylesheet" type="text/css" href="style.css" />
 	<h1>Equine Database Project</h1>
 	<hr>
 	<a href="forms.php">Forms</a> | <a href="tables.php">Archive</a>
