4d78d2046b3ad47c516e8bc11565824d0614e82b
Cameron Lydon <cwly222@uky.edu>
2019-12-09 21:19:02 -0500

    Moved style.css inclusion to <head>

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
 <footer>
-	<link rel="stylesheet" type="text/css" ref="style.css" />
 	<script type="text/javascript" src="custom.js"></script>
 </footer>
--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <head>
+<link rel="stylesheet" type="text/css" ref="style.css" />
 	<h1>Equine Database Project</h1>
 	<hr>
 	<a href="forms.php">Forms</a> | <a href="tables.php">Archive</a>
