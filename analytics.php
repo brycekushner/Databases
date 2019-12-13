2d816bcde859f9db2df2be4be9c99aae68d08ee5
Wimmerer <wrkimmerer@outlook.com>
2019-12-13 07:12:19 -0500

    Added to homepage

--- analytics.php
+++ analytics.php
@@ -1,6 +1,59 @@
-<html>
-    <?php include('header.php'); ?>
-    <body>
+<head><?php include('header.php');?></head>
+<body>
+<?php
+    $host = "localhost";
+    $dbname= "equine";
+    $username  = "root";
+    $password = "";
+    
+    function printTable($query, $table_headers = NULL)
+{
+  $fields = mysqli_fetch_fields($query);
+  $rows = mysqli_fetch_all($query);
+  echo "<h2>$f</h2>";
+  echo "<table id=\"simpleTable\">";
+  echo "<thead>";
+  echo "<tr>";
+  if (!is_null($table_headers)) {
+    foreach ($fields as $field) {
+      if (array_key_exists($field->name,$table_headers)) {
+        $name = $table_headers[$field->name];
+        echo "<th>$name</th>";
+      }
+      else {
+        $name = $field->name;
+        echo "<th>$name</th>";
+      }
+    }
+  }
+  else {
+    foreach ($fields as $field) {
+      $name = $field->name;
+      echo "<th>$name</th>";
+    }
+  }
+  echo "</tr>";
+  echo "</thead>";
+
+
+  foreach($rows as $row){
+    echo "<tr>";
+    foreach($row as $field => $value){
+      echo "<td>" . $value . "</td>";
+    }
+    echo "</tr>";
+  }
+
+  echo "</table>";
+}
+$mysqli = mysqli_connect($host, $username, $password, $dbname);
+
+$query = "SELECT Forelimb.radiocarpal,COUNT(*) as count FROM Forelimb GROUP BY Forelimb.radiocarpal ORDER BY count DESC;";
+print_r($query);
+$results = mysqli_query($mysqli,$query);
+printTable($results);
+    ?>
+
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">
         google.charts.load('current', {'packages':['bar']});
@@ -38,4 +91,3 @@
         <div id="forelimb_abnormality" style="width: 900px; height: 500px; padding: 30px;"></div>
         <div id="hindlimb_abnormality" style="width: 900px; height: 500px; padding: 30px;"></div>
     </body>
-</html>
