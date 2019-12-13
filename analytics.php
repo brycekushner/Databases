9e134b692d6358fe95ed0016cab5bf60e0ef11f8
Warthou <cameronlydon@live.com>
2019-12-12 21:50:27 -0500

    Google Charts

--- analytics.php
+++ analytics.php
@@ -1,1 +1,24 @@
-<?php ?>
+<html>
+    <?php include('header.php'); ?>
+    <body>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript">
+        google.charts.load('current', {packages: ['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Forelimb', 'Percentage'],
+                ['Left Forelimb', 1]
+                ['Right Forelimb', 2]
+            ]);
+            var options = {
+                title: 'Percent of abnormal pathology designations in each forelimb bone',
+                legend: { position: 'none' },
+            };
+            var chart = new google.visualization.Histogram(document.getElementById('chart_div'));
+            chart.draw(data, options);
+        }
+        </script>
+        <div id="chart_div" style="width: 900px; height: 500px;"></div>
+    </body>
+</html>
--- custom.js
+++ custom.js
@@ -1,17 +1,19 @@
 // Tab Section Functionality
 function openCity(evt, cityName){
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
 // // Set Login Cookie
 // function setCookie(name, value){
 //   document.cookie = name + "=" + (value || "") + "; path=/";	
