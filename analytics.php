41c4319739c22ffdfbea1b6a33a71b697ad6d9ba
Warthou <cameronlydon@live.com>
2019-12-12 22:28:28 -0500

    added Hindlimb graph

--- analytics.php
+++ analytics.php
@@ -1,25 +1,12 @@
 <html>
     <?php include('header.php'); ?>
     <body>
-    <?php
-        // Connects to SQL Database
-        // $host = 'localhost';
-        // $userName = 'root';
-        // $password = 'pwd';
-        // $DB = 'equine';
-        // $mysqli = mysqli_connect($host, $userName, $password, $DB);
-
-        // if(!$mysqli){
-        //     echo "Dead Connection: ".mysqli_connect_error();
-        // }else{
-        //     $first_histogram = ;
-        // }
-    ?>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">
         google.charts.load('current', {'packages':['bar']});
-        google.charts.setOnLoadCallback(drawChart);
-        function drawChart() {
+        google.charts.setOnLoadCallback(drawForelimbChart);
+        google.charts.setOnLoadCallback(drawHindlimbChart);
+        function drawForelimbChart() {
             var data = google.visualization.arrayToDataTable([
                 ['Breed', 'Fracture', 'Lysis', 'Sclerosis', 'Within Normal Limits'],
                 ['Thoroughbred', 20, 15, 10, 55],
@@ -33,7 +20,22 @@
             var chart = new google.charts.Bar(document.getElementById('forelimb_abnormality'));
             chart.draw(data, google.charts.Bar.convertOptions(options));
         }
+        function drawHindlimbChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Breed', 'Fracture', 'Lysis', 'Sclerosis', 'Within Normal Limits'],
+                ['Thoroughbred', 30, 15, 10, 45],
+                ['Arabian', 10, 10, 20, 60],
+                ['Quarter Horse', 5, 10, 5, 80],
+                ['Standardbred', 10, 5, 10, 75]
+            ]);
+            var options = {
+                title: 'Horse Breed vs Hindlimb Abnormality in Percentages'
+            };
+            var chart = new google.charts.Bar(document.getElementById('hindlimb_abnormality'));
+            chart.draw(data, google.charts.Bar.convertOptions(options));
+        }
         </script>
-        <div id="forelimb_abnormality" style="width: 900px; height: 500px;"></div>
+        <div id="forelimb_abnormality" style="width: 900px; height: 500px; padding: 30px;"></div>
+        <div id="hindlimb_abnormality" style="width: 900px; height: 500px; padding: 30px;"></div>
     </body>
 </html>
